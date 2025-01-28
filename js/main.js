69c9126d77be99fceb9f7a1028b861723196f9c1
Manuela Gomez <manuelagomezmgg@gmail.com>
2025-01-28 00:11:23 -0600

    agregue js

--- js/main.js
+++ js/main.js
@@ -14,30 +14,30 @@ const galleta1= {
 
 let nombreDelPedido = prompt("Bienvenido, por favor ingrese su nombre completo.")
 
-if (nombreDelPedido === null){
+if (nombreDelPedido === undefined){
     console.log("No agregaste un nombre a tu pedido")
 }
 
-let galletaSeleccionada = prompt(nombreDelPedido + ", porfavor ingresa  'galleta1' si deseas la galleta de triple chocolate , 'galleta2' si deseas la galleta de lemon pie  o  'galleta3' si deseas la galleta de macadamia'")
+let galletaSeleccionada = prompt(nombreDelPedido + ", porfavor ingresa: 'galleta1' si deseas la galleta de triple chocolate , 'galleta2' si deseas la galleta de lemon pie  o  'galleta3' si deseas la galleta de macadamia'")
 
-if (galleta1.precio){
+if (galletaSeleccionada===galleta1){
     console.log("La galleta de triple chocolate tiene un costo de ${galleta1.precio}"); 
-}else if (galleta2.precio){
+}else if (galletaSeleccionada===galleta2){
     console.log("La galleta de lemon pie tiene un costo de ${galleta2.precio}")
-}else if (galleta3.precio){
+}else if (galletaSeleccionada===galleta3){
     console.log("La galleta de macadamia tiene un costo de ${galleta3.precio}")
 }else{
     console.log("No tenemos esta opcion en nuestro inventario")
 }
     
-//cantidadGalletas
-let cantidadGalletas = parseInt(prompt ("Cuantas galletas deseas ordenar?"))
-
-
-function calcularTotal (cantidadGalletas){
-    return cantidadGalletas * galletaSeleccionada.precio
+let cantidadGalletas = parseInt(prompt ("Cuantas galletas deseas ordenar? Puedes elegir de 1 unidad a 30 unidades como maximo"))
+if (galletaSeleccionada>0 && galletaSeleccionada<=30){
+    let total = cantidadGalletas * galletaSeleccionada.precio
+    console.log(nombreDelPedido + " el total de tu pedido es de: $ ")
+}else{
+    console.log("Cantidad invalida")
 }
-console.log(nombreDelPedido + " el total de tu pedido es de: $ ")
+
 
 
 
